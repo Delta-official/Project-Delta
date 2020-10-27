6527310412926aeeeba9b6f41a91598dcb7aa54d
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-10-27 08:05:09 +0300

    made some magic

---
+++ PD_C/PD_C-LP.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "LP",
+    execute(messages, args) {
+        
+    }
+}
--- botmain.js
+++ botmain.js
@@ -1,2 +1,18 @@
 const Discord = require('discord.js')
 const Token = require('/token.js')
+
+const client = new Discord.client()
+client.commands = new Discord.Collection()
+
+const PD_C = fs.readdirSync("./PD_C").filter(file => file.endsWith(".js"));
+
+for(const file of PD_C) {
+    const command = require(`./PD_C/${file}`)
+    client.commands.set(command.name, command)
+}
+
+client.on('ready', () => {
+    console.log("Project ∆ active, awaiting input...")
+    client.user.setActivity("Stratzenblitz | s.help")
+
+})
