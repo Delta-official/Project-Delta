01f324e86f59b14f1969264d55ca4b58f301e1f2
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-10-29 02:11:11 +0300

    Update PD_C-LP.js

--- PD_C/PD_C-LP.js
+++ PD_C/PD_C-LP.js
@@ -2,15 +2,14 @@ module.exports = {
     name: "PD_C-LP",
     execute(messages, args) {
         if(message.guild.id === STRATZ_SERVER_ID) {
-            if(message.member.roles.cache.has(PATREON_ID, PATREONPLUS_ID)) {
+            if(message.member.roles.cache.has(PATREON_ID || PATREONPLUS_ID)) {
                 member.roles.add(Lpass)
                 message.channel.send('*Welcome to the Stratzen Lounge!*');
-        }
-        } else {
+        } else if(!message.member.roles.cache.has(PATREON_ID || PATREONPLUS_ID)) {
         message.channel.send("*You need to have a Patreon or Patreon+ role in order to get Lounge Pass!*")
-        }
-            if(message.member.roles.cache.has(LP_ID)) {
-                message.channel.send("*You already have Lounge Pass!")
-            }
+        } else if(message.member.roles.cache.has(LP_ID)) {
+            message.channel.send("*You already have Lounge Pass!")
+    }
+    } 
     }
 }
