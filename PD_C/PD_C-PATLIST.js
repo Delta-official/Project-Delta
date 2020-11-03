b30c340bee618c51daf80a692215a2cce41aea2d
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-11-03 04:09:24 +0300

    Update PD_C-PATLIST.js

--- PD_C/PD_C-PATLIST.js
+++ PD_C/PD_C-PATLIST.js
@@ -1,15 +1,12 @@
 const config = require("./config.json")
+const Discord = require("discord.js")
 
 module.exports = {
     name: "PD_C-PATLIST",
     execute(message, args) {
-        function PAT_L() {
-            message.guild.roles.cache.get(config.PATREON_ID).members.map(m=>m.user.tag).join("\n")
-        }
-        function PATplus_L() {
-            message.guild.roles.cache.get(config.PATREONPLUS_ID).members.map(m=>m.user.tag).join("\n")
-        }
-        message.channel.send(`**PATREON LIST**\n${message.guild.roles.cache.get(config.PATREON_ID).members.map(m=>m.user.tag).join("\n")}`)
-        message.channel.send(`**PATREON+ LIST**\n${message.guild.roles.cache.get(config.PATREONPLUS_ID).members.map(m=>m.user.tag).join("\n")}`)
+        const PAT_L_EMB = new Discord.MessageEmbed()
+        .addFields(
+            {name: "**PATREON LIST**", value: `${message.guild.roles.cache.get(config.PATREON_ID).members.map(m=>m.user.mention).join("\n")}`, inline: true},
+            {name: "**PATREON+ LIST", value: `${message.guild.roles.cache.get(config.PATREONPLUS_ID).members.map(m=>m.user.mention).join("\n")}`, inline: true});
     }
 }
