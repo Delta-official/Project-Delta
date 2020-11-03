f551469ab27e1179923e00fe7e3ad88256a4af9b
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-11-03 04:28:44 +0300

    Update PD_C-PATLIST.js

--- PD_C/PD_C-PATLIST.js
+++ PD_C/PD_C-PATLIST.js
@@ -5,9 +5,7 @@ module.exports = {
     name: "PD_C-PATLIST",
     execute(message, args) {
         const PAT_L_EMB = new Discord.MessageEmbed()
-        .addFields(
-            {name: "**PATREON LIST**", value: `${message.guild.roles.cache.get(config.PATREON_ID).members.map(m=>m.user.mention).join("\n")}`, inline: true},
-            {name: "**PATREON+ LIST", value: `${message.guild.roles.cache.get(config.PATREONPLUS_ID).members.map(m=>m.user.mention).join("\n")}`, inline: true});
+        .setDescription(`**PATREON LIST**\n${message.guild.roles.cache.get(config.PATREON_ID).members.map(m=>m.user.mention).join("\n")}\n**PATREON+ LIST**\n${message.guild.roles.cache.get(config.PATREONPLUS_ID).members.map(m=>m.user.mention).join("\n")}`)
         message.channel.send(PAT_L_EMB)
     }
 }
