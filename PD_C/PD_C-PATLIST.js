480c18a50c122e88431e17950230e34e60b006ec
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-11-03 04:14:07 +0300

    Update PD_C-PATLIST.js

--- PD_C/PD_C-PATLIST.js
+++ PD_C/PD_C-PATLIST.js
@@ -8,5 +8,6 @@ module.exports = {
         .addFields(
             {name: "**PATREON LIST**", value: `${message.guild.roles.cache.get(config.PATREON_ID).members.map(m=>m.user.mention).join("\n")}`, inline: true},
             {name: "**PATREON+ LIST", value: `${message.guild.roles.cache.get(config.PATREONPLUS_ID).members.map(m=>m.user.mention).join("\n")}`, inline: true});
+        message.channel.send(PAT_L_EMB)
     }
 }
