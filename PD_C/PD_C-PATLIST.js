5e9d2232f742f976dca64c32f8ab485120e2d053
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-11-03 03:10:30 +0300

    Update PD_C-PATLIST.js

--- PD_C/PD_C-PATLIST.js
+++ PD_C/PD_C-PATLIST.js
@@ -3,8 +3,12 @@ const config = require("./config.json")
 module.exports = {
     name: "PD_C-PATLIST",
     execute(message, args) {
-        var PAT_L = message.guild.roles.get(config.PATREON_ID).members.map(m=>m.user.tag).join("\n")
-        var PATplus_L = message.guild.roles.get(config.PATREONPLUS_ID).members.map(m=>m.user.tag).join("\n")
+        function PAT_L() {
+            message.guild.roles.get(config.PATREON_ID).members.map(m=>m.user.tag).join("\n")
+        }
+        function PATplus_L() {
+            message.guild.roles.get(config.PATREONPLUS_ID).members.map(m=>m.user.tag).join("\n")
+        }
         message.channel.send(`**PATREON LIST**\n${PAT_L}`)
         message.channel.send(`**PATREON+ LIST**\n${PATplus_L}`)
     }
